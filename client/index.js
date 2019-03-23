7de52fc3ed5deda0bc59f26a8191a58082053f79
26prajval98 <26prajval98@gmail.com>
2019-03-24 02:00:45 +0530

    added electron support

---
+++ client/index.js
@@ -0,0 +1,14 @@
+const electron = require('electron')
+
+const app = electron.app
+const window = electron.BrowserWindow
+
+app.commandLine.appendSwitch('ignore-certificate-errors', 'true');
+
+// When app is ready
+app.on('ready', ()=>{
+	// Create a new window
+	var mainWindow = new window({width : 1920, height : 1080});
+	mainWindow.setMenuBarVisibility(false);
+	mainWindow.loadURL('http://localhost:5000');
+})
